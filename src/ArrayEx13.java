abba3644b484218934fd47f49253b1d51951259c
AlekZam2 <lex.zam@hotmail.com>
2023-08-04 20:54:28 +0100

    tasks

--- src/ArrayEx13.java
+++ src/ArrayEx13.java
@@ -5,38 +5,34 @@ public class ArrayEx13 {
 
 
         String text = "abcde";
+        String[] s = text.split("");
 
-        String [] textArr = text.split("");
-        String [] newTextArr = new String[textArr.length];
+        for (int i = 0; i < s.length/2; i++) {
+            for (int j = s.length-1; j >= 0 ; j--) {
+                String temp = s[i];
 
-        String [] result = new String[textArr.length];
-        for (int i = textArr.length-1; i >= 0 ; i--) {
-            newTextArr[i] = textArr[i];
+                s[i] = s[j];
 
-        }
-
-
-
-        for (int i = 0; i < textArr.length/2; i++) {
-            result[i] = textArr[i];
-
-        }
-        for (int i = newTextArr.length/2 -1; i >=0 ; i++) {
-            result[i] = textArr[i];
-
-        }
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
+                s[j] = temp;
+            }
+            String reversedText = String.join("", s);
+            System.out.println("Reversed string: " + reversedText);
 
         }
 
+//        for (int i = 0, j = s.length - 1; i < s.length / 2; i++, j--) {
+//            // Use a temporary variable (temp) to store the character at index i.
+//            String temp = s[i];
+//
+//            // Replace the character at index i with the character at index j.
+//            s[i] = s[j];
+//
+//            // Replace the character at index j with the character in the temporary variable (temp).
+//            s[j] = temp;
+//        }
+//        String reversedText = String.join("", s);
+//        System.out.println("Reversed string: " + reversedText);
 
-
-
-
-
-
-        }
-
+    }
 
 }
